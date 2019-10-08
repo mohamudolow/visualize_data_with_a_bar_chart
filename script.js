042d4dd3110c22560d4ef34627b70ee78fbc486e
Mohamud Olow <khalif18@live.com>
2019-10-08 09:00:45 -0700

    set the x and y axes of the graph

--- script.js
+++ script.js
@@ -1,37 +1,58 @@
-const w = 500,
-      h = 500,
-      padding = 40;
+const w = 800,
+      h = 500;
 
 const margin = {top: 50, right: 30, bottom: 20, left: 30};
 
 function drawBar(dataset) {
-    
-    let minDate = d3.min(dataset, d => d[0].substr(0, 4));
-            minDate = new Date(minDate);
-    
-    let maxDate = d3.max(dataset, d => d[1]);
-            maxDate = new Date(maxDate);
-    
-    let xAxisScale = d3.scaleTime()
-                    .domain([minDate, maxDate])
-                    .range([0, w]);
-    
-     let yAxisScale = d3.scaleLinear()
-                        .domain([0, d3.max(dataset, d => d[1])])
-                        .range([h, 0]);
-    
-    let xAxis = d3.axisBottom(xAxisScale);
-    let yAxis = d3.axisLeft(xAxisScale);
-    
-  
+
     const svg = d3.select("#bar-chart")
-                .append("svg")
-                .style("background-color", "gray")
-                .attr("width", w + margin.left + margin.right)
-                .attr("height", h + margin.top + margin.bottom)
-                .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.right + ")");
+    .append("svg")
+    .style("background-color", "gray")
+    .attr("width", w +margin.left + margin.right)
+    .attr("height", h + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.top + "," + margin.bottom + ")");
+
+    let yearsDate = dataset.map(item => {
+        return item[0];
+    });
+
+    let minDate = new Date(d3.min(yearsDate, d => d));
+
+    let maxDate = new Date(d3.max(yearsDate, d => d));
+    maxDate.setMonth(maxDate.getMonth() + 3);
+
+    let xAxisScale = d3.scaleTime()
+    .domain([minDate, maxDate])
+    .range([0, w]);
+
+    let xAxis = d3.axisBottom().scale(xAxisScale);
+
+    let xAxisGroup = svg.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", "translate(0, " + h + ")")
+    .call(xAxis);
+
+    let gdp = dataset.map(item => item[1]);
     
+    gdp.push(gdp[gdp.length-1] + (20000 - gdp[gdp.length-1]));
+    
+    let maxGdp = d3.max(gdp, d => d);
+    
+    console.log(gdp);
+    
+    let yAxisScale = d3.scaleLinear()
+    .domain([0, maxGdp])
+    .range([h, 0]);
+
+    let yAxis = d3.axisLeft(yAxisScale);
+
+    let yAxisGroup = svg.append("g")
+    .attr("class", "y-axis")
+    .attr("transform", "translate(" + w + ", 0)")
+    .attr("transform", "rotate(0)")
+    .call(yAxis);
+
     svg.selectAll("rect")
         .data(dataset)
         .enter()
@@ -41,13 +62,13 @@ function drawBar(dataset) {
         .attr("y", d => yAxisScale(d[1]))
         .attr("width", d => w/dataset.length)
         .attr("height", d => h - yAxisScale(d[1]));
+
 }
+
 
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json").then((data) => {
     var dataset = data.data;
-    console.log(dataset.length);
     drawBar(dataset);
 });
 
-//d3.min(dataset, d => d[1]), [d3.max(dataset, d => d[1])]
 
